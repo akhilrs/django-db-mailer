18fc2301cf8a3df4e1e81011cbe9699f2df1cd21
akhil.rs <hello@akhil.rs>
2025-01-08 23:46:20 +0530

    update package version

--- dbmail/__init__.py
+++ dbmail/__init__.py
@@ -1,16 +1,15 @@
 # -*- encoding: utf-8 -*-
 
-from datetime import datetime
 import sys
+from datetime import datetime
 
+VERSION = (2, 5, 4)
 
-VERSION = (2, 4, 0)
-
-default_app_config = 'dbmail.apps.DBMailConfig'
+default_app_config = "dbmail.apps.DBMailConfig"
 
 
 def get_version():
-    return '.'.join(map(str, VERSION))
+    return ".".join(map(str, VERSION))
 
 
 def app_installed(app):
@@ -30,36 +29,42 @@ def celery_supported():
 
 def db_sender(slug, recipient, *args, **kwargs):
     from dbmail.defaults import (
-        CELERY_QUEUE, SEND_MAX_TIME, ENABLE_CELERY, BACKEND, DEBUG)
+        BACKEND,
+        CELERY_QUEUE,
+        DEBUG,
+        ENABLE_CELERY,
+        SEND_MAX_TIME,
+    )
     from dbmail.models import MailTemplate
 
     args = (slug, recipient) + args
-    send_after = kwargs.pop('send_after', None)
-    send_at_date = kwargs.pop('send_at_date', None)
-    _use_celery = kwargs.pop('use_celery', ENABLE_CELERY)
+    send_after = kwargs.pop("send_after", None)
+    send_at_date = kwargs.pop("send_at_date", None)
+    _use_celery = kwargs.pop("use_celery", ENABLE_CELERY)
     use_celery = ENABLE_CELERY and _use_celery
-    backend = kwargs.get('backend', BACKEND['mail'])
+    backend = kwargs.get("backend", BACKEND["mail"])
 
     if celery_supported() and use_celery is True:
         import dbmail.tasks
 
         template = MailTemplate.get_template(slug=slug)
-        max_retries = kwargs.get('max_retries', None)
+        max_retries = kwargs.get("max_retries", None)
         send_after = send_after if send_after else template.interval
         if max_retries is None and template.num_of_retries:
-            kwargs['max_retries'] = template.num_of_retries
+            kwargs["max_retries"] = template.num_of_retries
 
         options = {
-            'args': args, 'kwargs': kwargs,
-            'queue': kwargs.pop('queue', CELERY_QUEUE),
-            'time_limit': kwargs.get('time_limit', SEND_MAX_TIME),
-            'priority': template.priority,
+            "args": args,
+            "kwargs": kwargs,
+            "queue": kwargs.pop("queue", CELERY_QUEUE),
+            "time_limit": kwargs.get("time_limit", SEND_MAX_TIME),
+            "priority": template.priority,
         }
 
         if send_at_date is not None and isinstance(send_at_date, datetime):
-            options.update({'eta': send_at_date})
+            options.update({"eta": send_at_date})
         if send_after is not None:
-            options.update({'countdown': send_after})
+            options.update({"countdown": send_after})
         if template.is_active:
             return dbmail.tasks.db_sender.apply_async(**options)
     else:
@@ -72,54 +77,58 @@ def db_sender(slug, recipient, *args, **kwargs):
 def send_db_mail(*args, **kwargs):
     from dbmail.defaults import BACKEND
 
-    kwargs['backend'] = kwargs.pop('backend', BACKEND['mail'])
+    kwargs["backend"] = kwargs.pop("backend", BACKEND["mail"])
     return db_sender(*args, **kwargs)
 
 
 def send_db_sms(*args, **kwargs):
     from dbmail.defaults import BACKEND, SMS_QUEUE
 
-    kwargs['backend'] = kwargs.pop('backend', BACKEND['sms'])
-    kwargs['queue'] = kwargs.pop('queue', SMS_QUEUE)
+    kwargs["backend"] = kwargs.pop("backend", BACKEND["sms"])
+    kwargs["queue"] = kwargs.pop("queue", SMS_QUEUE)
     return db_sender(*args, **kwargs)
 
 
 def send_db_tts(*args, **kwargs):
     from dbmail.defaults import BACKEND, TTS_QUEUE
 
-    kwargs['backend'] = kwargs.pop('backend', BACKEND['tts'])
-    kwargs['queue'] = kwargs.pop('queue', TTS_QUEUE)
+    kwargs["backend"] = kwargs.pop("backend", BACKEND["tts"])
+    kwargs["queue"] = kwargs.pop("queue", TTS_QUEUE)
     return db_sender(*args, **kwargs)
 
 
 def send_db_push(*args, **kwargs):
     from dbmail.defaults import BACKEND, PUSH_QUEUE
 
-    kwargs['backend'] = kwargs.pop('backend', BACKEND['push'])
-    kwargs['queue'] = kwargs.pop('queue', PUSH_QUEUE)
+    kwargs["backend"] = kwargs.pop("backend", BACKEND["push"])
+    kwargs["queue"] = kwargs.pop("queue", PUSH_QUEUE)
     return db_sender(*args, **kwargs)
 
 
 def send_db_bot(*args, **kwargs):
     from dbmail.defaults import BACKEND, BOT_QUEUE
 
-    kwargs['backend'] = kwargs.pop('backend', BACKEND['bot'])
-    kwargs['queue'] = kwargs.pop('queue', BOT_QUEUE)
+    kwargs["backend"] = kwargs.pop("backend", BACKEND["bot"])
+    kwargs["queue"] = kwargs.pop("queue", BOT_QUEUE)
     return db_sender(*args, **kwargs)
 
 
 def send_db_subscription(*args, **kwargs):
     from dbmail.defaults import (
-        SUBSCRIPTION_QUEUE, SEND_MAX_TIME,
-        ENABLE_CELERY, MAIL_SUBSCRIPTION_MODEL)
+        ENABLE_CELERY,
+        MAIL_SUBSCRIPTION_MODEL,
+        SEND_MAX_TIME,
+        SUBSCRIPTION_QUEUE,
+    )
 
     MailSubscription = import_by_string(MAIL_SUBSCRIPTION_MODEL)
 
-    use_celery = ENABLE_CELERY and kwargs.pop('use_celery', ENABLE_CELERY)
+    use_celery = ENABLE_CELERY and kwargs.pop("use_celery", ENABLE_CELERY)
     options = {
-        'time_limit': kwargs.pop('time_limit', SEND_MAX_TIME),
-        'queue': kwargs.pop('queue', SUBSCRIPTION_QUEUE),
-        'args': args, 'kwargs': kwargs,
+        "time_limit": kwargs.pop("time_limit", SEND_MAX_TIME),
+        "queue": kwargs.pop("queue", SUBSCRIPTION_QUEUE),
+        "args": args,
+        "kwargs": kwargs,
     }
 
     if celery_supported() and use_celery is True:
@@ -127,16 +136,22 @@ def send_db_subscription(*args, **kwargs):
 
         return db_subscription.apply_async(**options)
     else:
-        kwargs['use_celery'] = use_celery
+        kwargs["use_celery"] = use_celery
         return MailSubscription.notify(*args, **kwargs)
 
 
 def initial_signals():
     from django.db.utils import DatabaseError, IntegrityError
 
-    for cmd in ['schemamigration', 'migrate',
-                'test', 'createsuperuser', 'makemigrations',
-                'collectstatic', 'compilemessages']:
+    for cmd in [
+        "schemamigration",
+        "migrate",
+        "test",
+        "createsuperuser",
+        "makemigrations",
+        "collectstatic",
+        "compilemessages",
+    ]:
         if cmd in sys.argv:
             break
     else:
@@ -164,12 +179,13 @@ def python_2_unicode_compatible(klass):
     returning text and apply this decorator to the class.
     """
     if PY2:
-        if '__str__' not in klass.__dict__:
-            raise ValueError("@python_2_unicode_compatible cannot be applied "
-                             "to %s because it doesn't define __str__()." %
-                             klass.__name__)
+        if "__str__" not in klass.__dict__:
+            raise ValueError(
+                "@python_2_unicode_compatible cannot be applied "
+                "to %s because it doesn't define __str__()." % klass.__name__
+            )
         klass.__unicode__ = klass.__str__
-        klass.__str__ = lambda self: self.__unicode__().encode('utf-8')
+        klass.__str__ = lambda self: self.__unicode__().encode("utf-8")
     return klass
 
 
